4ca22bb354f6c2ed36deb11f1abf01c01e7ddd5c
hopo55 <hopo5798@gmail.com>
2022-07-27 01:50:37 +0900

    HS

--- PC/FaceRecognition/GEM/utils/util.py
+++ PC/FaceRecognition/GEM/utils/util.py
@@ -1,7 +1,11 @@
+from asyncio.staggered import staggered_race
+from cProfile import label
+from cgi import print_arguments
 import os
 import cv2
 from cv2 import transform
 from numpy import imag
+from pandas import value_counts
 import torch
 from sklearn import preprocessing
 from torch.utils.data import Dataset
@@ -60,11 +64,39 @@ class ImageDataset(IDataset):
 
         return image, target
 
-def PathDataset(dataset, transfrom, mode):
-    for exp_id, list_of_files in enumerate(dataset):
+def PathDataset(dataset, transfrom, mode, task_labels):
+    for list_of_files in dataset:
+        common_root, exp_paths_list = common_paths_root(list_of_files)
+        paths_dataset = PathsDataset(common_root, exp_paths_list)
+        stream_datasets = AvalancheDataset(paths_dataset, transform_groups=transfrom, initial_transform_group=mode, task_labels=task_labels)
+
+    return stream_datasets
+
+
+def PathDataset_v2(dataset, transfrom, mode):
+    for list_of_files in dataset:
         common_root, exp_paths_list = common_paths_root(list_of_files)
         paths_dataset = PathsDataset(common_root, exp_paths_list)
         stream_datasets = AvalancheDataset(paths_dataset, transform_groups=transfrom, initial_transform_group=mode)
 
     return stream_datasets
     
+def make_task(num_task, labels):
+    start = 0
+    max_label = max(labels)
+    task_count = max_label // num_task
+    task_labels = labels
+
+    for n_task in range(num_task):
+        for idx, value in enumerate(labels):
+            if value in range(start, task_count*(n_task+1)) and n_task < (num_task-1):
+                task_labels[idx] = n_task
+            elif n_task == (num_task-1) and value >= start:
+                task_labels[idx] = n_task
+            # if value >= start and value < task_count*(n_task+1) and n_task < 4:
+            #     task_labels[idx] = n_task
+            # else:
+            #     task_labels[idx] = n_task
+        start = task_count*(n_task+1)
+
+    return task_labels
